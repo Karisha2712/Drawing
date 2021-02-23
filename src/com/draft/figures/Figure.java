7ed79a65712980f01e41345a0b981c13c77e571b
Karina Radyuk <radyuk.karina@bk.ru>
2021-02-23 22:17:15 +0300

    second commit

--- src/com/draft/figures/Figure.java
+++ src/com/draft/figures/Figure.java
@@ -1,4 +1,38 @@
 package com.draft.figures;
 
-public class Figure {
+import java.awt.Point;
+
+public abstract class Figure {
+
+    private Point startPoint;
+    private Point endPoint;
+
+    public Figure(){
+        startPoint = new Point();
+        endPoint = new Point();
+    }
+
+    public Figure(final Point startPoint, final Point endPoint){
+        this.startPoint = new Point(startPoint);
+        this.endPoint = new Point(endPoint);
+    }
+
+    public void setStartPoint(final Point startPoint){
+        this.startPoint = startPoint;
+    }
+
+    public Point getStartPoint(){
+        return startPoint;
+    }
+
+    public void setEndPoint(final Point endPoint){
+        this.endPoint = endPoint;
+    }
+
+    public Point getEndPoint(){
+        return endPoint;
+    }
+
+    public abstract void draw();
+
 }
